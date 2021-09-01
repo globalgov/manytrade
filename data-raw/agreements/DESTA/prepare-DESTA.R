02e47c737d23506c4a231f784753d9a49510cb28
Jael Tan <jaeltan2304@gmail.com>
2021-09-01 16:27:26 +0200

    Imported DESTA dataset into package qTrade

---
+++ data-raw/agreements/DESTA/prepare-DESTA.R
@@ -0,0 +1,36 @@
+# DESTA Preparation Script
+
+# This is a template for importing, cleaning, and exporting data
+# ready for the qPackage.
+
+# Stage one: Collecting data
+DESTA <- readxl::read_excel("data-raw/agreements/DESTA/DESTA.xlsx")
+
+# Stage two: Correcting data
+# In this stage you will want to correct the variable names and
+# formats of the 'DESTA' object until the object created
+# below (in stage three) passes all the tests.
+DESTA <- as_tibble(DESTA) %>%
+  qData::transmutate(ID = {id_variable_name_here},
+              Beg = qCreate::standardise_dates({date_variable_name_here})) %>%
+  dplyr::arrange(Beg)
+# qCreate includes several functions that should help cleaning
+# and standardising your data.
+# Please see the vignettes or website for more details.
+
+# Stage three: Connecting data
+# Next run the following line to make DESTA available
+# within the qPackage.
+qCreate::export_data(DESTA, database = "agreements")
+# This function also does two additional things.
+# First, it creates a set of tests for this object to ensure adherence
+# to certain standards.You can hit Cmd-Shift-T (Mac) or Ctrl-Shift-T (Windows)
+# to run these tests locally at any point.
+# Any test failures should be pretty self-explanatory and may require
+# you to return to stage two and further clean, standardise, or wrangle
+# your data into the expected format.
+# Second, it also creates a documentation file for you to fill in.
+# Please note that the export_data() function requires a .bib file to be
+# present in the data_raw folder of the package for citation purposes.
+# Therefore, please make sure that you have permission to use the dataset
+# that you're including in the package.
