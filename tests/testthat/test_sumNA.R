d15ca1986f8e78d4f644d7b930e6421340346036
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-09-03 17:02:03 +0200

    Minor updates to tests for sumNA function

--- tests/testthat/test_sumNA.R
+++ tests/testthat/test_sumNA.R
@@ -1,4 +1,10 @@
 test_that("dataset name is declared", {
-  expect_error(sumNA(), "You need to name the dataset. We suggest a short, unique name,
-         all capital letters, such as 'DESTA'.")
+  expect_error(sumNA(), "Please declare a dataset")
+})
+
+data <- data.frame(a = c("a", NA),
+                   b = c(NA, NA))
+
+test_that("SumNA function works", {
+  expect_equal(as.character(sumNA(data)), c("1", "2"))
 })
