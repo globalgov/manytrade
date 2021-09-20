0756fcaa5298557576df584951f6af385c79aa9d
Jael Tan <jaeltan2304@gmail.com>
2021-09-20 12:06:08 +0200

    align variable names with qEnviron, add ID for GPTAD

--- R/qTrade-agreements.R
+++ R/qTrade-agreements.R
@@ -7,12 +7,12 @@
 #' functions.
 #'\describe{
 #' \item{DESTA: }{A dataset with 987 observations and the following
-#' 10 variables: DESTA_ID, Title, Beg, Signature, Force, Agreement type, Document type, Geographic area, WTO, qID.}
+#' 10 variables: DESTA_ID, Title, Beg, Signature, Force, D, L, J, WTO, qID.}
 #' \item{TREND: }{A dataset with 730 observations and the following
 #' 6 variables: TREND_ID, Title, Beg, Signature, Force, qID.}
 #' \item{LABPTA: }{A dataset with 483 observations and the following
 #' 6 variables: LABPTA_ID, Title, Beg, Signature, Force, qID.}
 #' \item{GPTAD: }{A dataset with 346 observations and the following
-#' 8 variables: Title, Beg, Signature, Force, Agreement type, Document type, WTO, qID.}
+#' 9 variables: GPTAD_ID, Title, Beg, Signature, Force, D, L, WTO, qID.}
 #' }
  "agreements"
