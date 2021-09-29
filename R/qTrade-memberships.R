c5106af1bcf29bdcf07a225fcd78913d461369f3
BBieri <bernhard.bieri@graduateinstitute.ch>
2021-09-29 14:37:33 +0200

    Fixed #12 by adding correctly formatted reference section to pkgdown site

--- R/qTrade-memberships.R
+++ R/qTrade-memberships.R
@@ -1,4 +1,4 @@
-#' memberships database documentation
+#' Memberships database documentation
 #'
 #' @format The memberships database is a list that contains the
 #' following 2 datasets: GPTAD_MEM, DESTA_MEM.
