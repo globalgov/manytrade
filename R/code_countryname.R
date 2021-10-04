456362435599d38603c1358dca9123cdf54dd96d
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-10-04 12:47:29 +0200

    Updated code_countryname() function to work with ACSII characters

--- R/code_countryname.R
+++ R/code_countryname.R
@@ -1,19 +1,19 @@
 #' Code Country Names
-#' 
-#' Standardize country names from other languages, abbreviations, or codes
 #'
+#' Standardize country names from other languages, abbreviations, or codes.
 #' @param name A character vector of country name
-#'
 #' @return A character vector of country name
-#' @export
-#'
 #' @examples
+#' name <- c("argentine", "albanie", "Bélarus")
+#' code_countryname(name)
+#' @export
 code_countryname <- function(name) {
-  
-  name <- as.character(name)
+  # Tranlates string to ASCII
+  name <- iconv(name, to="ASCII//TRANSLIT")
+  name <- data.frame(name = as.character(name))
   coment <- sapply(countryregex[, 3], function(x) grepl(x, name,
-                                                      ignore.case = T,
-                                                      perl = T) * 1)
+                                                        ignore.case = TRUE,
+                                                        perl = TRUE) * 1)
   colnames(coment) <- countryregex[, 2]
   rownames(coment) <- name
   out <- apply(coment, 1, function(x) paste(names(x[x == 1])))
