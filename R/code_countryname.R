57ca7b2bbd8aed0050c14ee786dbcba35f7f0c95
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-10-05 09:36:57 +0200

    Updated code_countryname() function by changing how the function calls country regex list and added abbreviations as an argument to the function. code_countryabbrev() was removed.

--- R/code_countryname.R
+++ R/code_countryname.R
@@ -1,47 +1,35 @@
 #' Code Country Names
 #'
-#' Standardize country names from other languages.
+#' Standardize spellings for country names,
+#' as well as country names in other languages.
 #' @param name A character vector of country name
+#' @param abbrev Do you want 3 letter country
+#' abbreviations to be returned?
+#' False by default.
+#' @importFrom stringi stri_trans_general
 #' @return A character vector of country name
 #' @examples
 #' name <- c("argentine", "albanie", "Bélarus")
 #' code_countryname(name)
+#' code_countryname(name, abbrev = TRUE)
 #' @export
-code_countryname <- function(name) {
+code_countryname <- function(name, abbrev = FALSE) {
   # Translates string to ASCII
   name <- stringi::stri_trans_general(name, "Latin-ASCII")
-  name <- data.frame(name = as.character(name))
-  coment <- data.frame(coment = sapply(countryregex[, 3], function(x) grepl(x, name,
-                                                                            ignore.case = TRUE,
-                                                                            perl = TRUE) * 1))
-  colnames(coment) <- name
-  rownames(coment) <- countryregex[, 2]
-  out <- apply(coment, 2, function(x) paste(names(x[x == 1])))
-  out[out == ""] <- NA
-  country <- unname(out)
-  country
-}
-
-#' Code Abbreviations of Country Names
-#' 
-#' Insert abbreviations of country names.
-#' @param name 
-#' @return A character vector of an abbreviation of specified country name
-#' @examples
-#' name <- c("argentine", "albanie", "Bélarus")
-#' code_countryname(name)
-#' @export
-code_countryabbrv <- function(name) {
-  # Translates string to ASCII
-  name <- stringi::stri_trans_general(name, "Latin-ASCII")
-  name <- data.frame(name = as.character(name))
-  coment <- data.frame(coment = sapply(countryregex[, 2], function(x) grepl(x, name,
-                                                                            ignore.case = TRUE,
-                                                                            perl = TRUE) * 1))
-  colnames(coment) <- name
-  rownames(coment) <- countryregex[, 1]
-  out <- apply(coment, 2, function(x) paste(names(x[x == 1])))
-  out[out == ""] <- NA
-  abbrv <- unname(out)
-  abbrv
+  if (abbrev == TRUE) {
+    for (k in seq_len(nrow(countryregex))) {
+      name <- gsub(paste0(countryregex$Regex[[k]]),
+                   paste0(countryregex$StatID[[k]]),
+                   name, ignore.case = TRUE,
+                   perl = T)
+      }
+  } else {
+      for (k in seq_len(nrow(countryregex))) {
+        name <- gsub(paste0(countryregex$Regex[[k]]),
+                     paste0(countryregex$Label[[k]]),
+                     name, ignore.case = TRUE,
+                     perl = T)
+        }
+  }
+  name
 }
