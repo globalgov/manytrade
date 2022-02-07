09e116a88c029e460a46637e3cd128a66215174c
EstherPeev <esther.peev@graduateinstitute.ch>
2022-02-07 11:34:00 +0100

    Changed function to scrape information into texts database for TOTA dataset

--- R/manytrade-texts.R
+++ R/manytrade-texts.R
@@ -1,16 +1,19 @@
 #' texts database documentation
 #'
 #' @format The texts database is a list that contains the
-#' following 1 datasets: AGR_TXT.
+#' following 2 datasets: AGR_TXT, TOTA_TXT.
 #' For more information and references to each of the datasets used,
 #' please use the `data_source()` and `data_contrast()` functions.
 #'\describe{
 #' \item{AGR_TXT: }{A dataset with 352 observations and the following
 #' 3 variables: Title, Beg, TreatyText.}
+#' \item{TOTA_TXT: }{A dataset with 450 observations and the following
+#' 2 variables: ID, TreatyText.}
 #' }
-#' 
+
+#'
 #' @details
 #' ``` {r, echo = FALSE, warning = FALSE}
 #' lapply(texts, skimr::skim_without_charts)
 #' ```
- "texts"
+"texts"
