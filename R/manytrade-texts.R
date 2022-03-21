25d88b3e9c4169d79bc0b1d2c399560aabaad9d8
Jael Tan <jael.tan@graduateinstitute.ch>
2022-03-21 11:47:22 +0100

    Removed AGR_TXT documentation and tests

--- R/manytrade-texts.R
+++ R/manytrade-texts.R
@@ -1,12 +1,10 @@
 #' texts database documentation
 #'
 #' @format The texts database is a list that contains the
-#' following 2 datasets: AGR_TXT, TOTA_TXT.
+#' following dataset: TOTA_TXT.
 #' For more information and references to each of the datasets used,
 #' please use the `data_source()` and `data_contrast()` functions.
 #'\describe{
-#' \item{AGR_TXT: }{A dataset with 1170 observations and the following
-#' 9 variables: manyID, Title, Beg, Signature, Force, treatyID, destaID, gptadID, TreatyText.}
 #' \item{TOTA_TXT: }{A dataset with 450 observations and the following
 #' 8 variables: manyID, Title, Beg, Signature, Force, TreatyText, treatyID, url.}
 #' }
