75ce029ff2a4c1b20419e32cec49b356b03bd5e0
Jael Tan <jael.tan@graduateinstitute.ch>
2022-03-23 07:48:57 +0100

    linted package

--- R/manytrade-memberships.R
+++ R/manytrade-memberships.R
@@ -6,9 +6,11 @@
 #' please use the `data_source()` and `data_contrast()` functions.
 #'\describe{
 #' \item{GPTAD_MEM: }{A dataset with 2201 observations and the following
-#' 9 variables: manyID, CountryID, Title, Beg, Signature, Force, CountryName, gptadID, treatyID.}
+#' 9 variables: manyID, CountryID, Title, Beg, Signature, Force,
+#' CountryName, gptadID, treatyID.}
 #' \item{DESTA_MEM: }{A dataset with 7492 observations and the following
-#' 9 variables: manyID, CountryID, Title, Beg, Signature, Force, CountryName, destaID, treatyID.}
+#' 9 variables: manyID, CountryID, Title, Beg, Signature, Force,
+#' CountryName, destaID, treatyID.}
 #' }
 
 #'
