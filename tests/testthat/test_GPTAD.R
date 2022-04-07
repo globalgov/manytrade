45f85ba23f14ae052bd71fef2894d7e51cd38c07
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2022-04-07 10:13:26 +0200

    Added one missing pointblank call in tests

--- tests/testthat/test_GPTAD.R
+++ tests/testthat/test_GPTAD.R
@@ -14,8 +14,8 @@ test_that("missing observations are reported correctly", {
 # entry into force date)
 test_that("datasets have the required variables", {
   pointblank::expect_col_exists(agreements[["GPTAD"]], pointblank::vars(Title))
-  expect_col_exists(agreements[["GPTAD"]], pointblank::vars(Beg))
-  pointblank::expect_true(any(grepl("ID$", colnames(agreements[["GPTAD"]]))))
+  pointblank::expect_col_exists(agreements[["GPTAD"]], pointblank::vars(Beg))
+  expect_true(any(grepl("ID$", colnames(agreements[["GPTAD"]]))))
   pointblank::expect_col_exists(agreements[["GPTAD"]], pointblank::vars(Signature))
 })
 
