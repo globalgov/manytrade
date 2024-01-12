5ee56a690890578a00ba17b17678b4fac98083d9
Jael Tan <jael.tan@graduateinstitute.ch>
2024-01-12 18:21:58 +0100

    Updated HUGGO_MEM test

--- tests/testthat/test_HUGGO_MEM.R
+++ tests/testthat/test_HUGGO_MEM.R
@@ -42,9 +42,9 @@ test_that("Column `Begin` has standardised dates", {
 # Dataset should be ordered according to the "Begin" column
 test_that("dataset is arranged by the `Begin` variable", {
   expect_true(memberships[["HUGGO_MEM"]]$Begin[1] <
-                memberships[["HUGGO_MEM"]]$Begin[100])
+                memberships[["HUGGO_MEM"]]$Begin[200])
   expect_true(memberships[["HUGGO_MEM"]]$Begin[120] <
-                memberships[["HUGGO_MEM"]]$Begin[220])
-  expect_true(memberships[["HUGGO_MEM"]]$Begin[250] <
-                memberships[["HUGGO_MEM"]]$Begin[350])
+                memberships[["HUGGO_MEM"]]$Begin[420])
+  expect_true(memberships[["HUGGO_MEM"]]$Begin[850] <
+                memberships[["HUGGO_MEM"]]$Begin[1050])
 })
